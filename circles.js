93f180e467b5a13a84966b83e2d7b15c207e595b
Namir Sayed-Ahmad Baraza <namir.sab@quantilope.com>
2017-05-15 23:12:27 +0200

    Finish first version of circles

--- circles.js
+++ circles.js
@@ -4,8 +4,6 @@ var canvas;
 var barWidth;
 var maxWidth;
 var maxHeight;
-var yellowFountain;
-var redFountain;
 var peakFountain;
 var peakFountain2;
 var peakDetect;
@@ -17,21 +15,33 @@ function preload() {
 
 }
 
+var prevParticle = { location: { x: 0, y: 0 }};
+function myPoint(fountain, particle) {
+
+    stroke(color(255, random(255), random(255)));
+    strokeWeight(particle.partSize);
+    line(particle.location.x, particle.location.y, prevParticle.location.x, prevParticle.location.y);
+    prevParticle = particle;
+}
+
 function setup() {
     canvas = createCanvas(maxWidth, maxHeight, P2D);
     // Maptastic(canvas.id());
     print(canvas);
     fft = new p5.FFT();
-    peakDetect = new p5.PeakDetect(20, 20000, 0.35, 60);
+    peakDetect = new p5.PeakDetect(20, 20000, 0.10, 60);
     song.start();
     fft.setInput(song);
+
+    Fountain_display('text', ftext);
+    Fountain_display('line', myPoint);
 
     var t =
     {
         name: "test",
         colors: ["yellow"],
         gravity: 0.00,
-        lifetime: 900,
+        lifetime: 400,
         angle: [0,360],
         size: [2,8],
         rate: [1, 1],
@@ -41,37 +51,32 @@ function setup() {
         y: 0.5,
         rotation: 0,
     };
-    yellowFountain = new Fountain(null, t);
-
-    redFountain = new Fountain(null, Object.assign({}, t, {
-        colors: ['red'],
-        x: 1,
-        y: 0.5
-    }));
 
     peakFountain = new Fountain(null, Object.assign({}, t, {
-        colors: [color('rgba(0, 0, 255, 255)')],
+        colors: ['yellow'],
         x: 0.5,
         y: 0.5,
-        rate: [1,300],
+        rate: [1,200],
         angle: [0, 360],
-        size: [10, 15],
+        size: [1, 15],
         speedx: 0.2,
         speed: 3,
-        file: './assets/swirl-colorful-rainbow-colors-arts-colors-circle.png',
-        shape: 'image',
     }));
 
     peakFountain2 = new Fountain(null, Object.assign({}, t, {
         colors: ['blue'],
         x: 0.5,
         y: 0.5,
-        rate: [1,300],
+        lifetime: 500,
+        rate: [2,7],
         angle: [0, 360],
-        size: [10, 15],
+        size: [3, 18],
         speedx: 0.2,
         speed: 3,
+        shape: 'line'
     }));
+
+    
 
     
 }
@@ -82,11 +87,18 @@ function draw() {
     peakDetect.update(fft);
     var bassEnergy = fft.getEnergy('bass');
     var lowMid = fft.getEnergy('lowMid');
+    var treble = fft.getEnergy('treble');
     background(0);
     noStroke();
-    renderCircle(color(255,0,0), lowMid, { x: maxWidth/2, y: maxHeight/2})
+    renderCircle(color(bassEnergy,0,0), bassEnergy, { x: maxWidth/2, y: maxHeight/2})
+    renderCircle(color(0,lowMid,0), lowMid, { x: maxWidth/4, y: maxHeight/2})
+    renderCircle(color(0,lowMid,0), lowMid, { x: maxWidth - maxWidth/4, y: maxHeight/2})
+    renderCircle(color(0,0,treble), treble, { x: maxWidth/2, y: maxHeight/4})
+    renderCircle(color(0,0,treble), treble, { x: maxWidth/2, y: maxHeight - maxHeight/4})
     peakFountain.f.speed = map(bassEnergy, 0, 255, 0, 10);
     peakFountain2.f.speed = map(lowMid, 0, 255, 0, 10);
+    peakFountain.f.gravity = map(bassEnergy, 0, 255, -0.3, 0.1);
+    peakFountain2.f.gravity = map(lowMid, 0, 255, -0.3, 0.1);
     peakFountain.Draw();
     peakFountain2.Draw();
     if (peakDetect.isDetected) {
@@ -110,11 +122,4 @@ function renderCircle(color, energy, { x, y }, fountain) {
     fill(color);
     const size = map(energy, 0, 255, 10, maxHeight/2);
     ellipse(x, y, size, size);
-
-    /*fountain.f.speed = map(energy, 0, 255, 1, 3);
-    fountain.f.gravity = map(energy, 0, 255, -1, 0.3);
-    fountain.f.speedx = map(energy, 0, 255, 0, 6) ;
-    fountain.Draw();
-    fountain.Create();
-    fountain.Step();*/
 }
